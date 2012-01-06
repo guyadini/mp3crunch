323f8790e69149c205167eba019dbaf2392803b1
Guy Adini <guyadini@gmail.com>
2012-01-06 13:30:35 +0200

    A preliminary version, which just prints out the file names

--- crunch.py
+++ crunch.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python
 #-------------------------------------------------------------------------------
 # Name:        crunch
-# Purpose:
 #
 # Author:      guy
 #
 # Created:     06/01/2012
-# Copyright:   (c) guy 2012
-# Licence:     <your licence>
+
 #-------------------------------------------------------------------------------
 
 import os
 import sys
+#tagger is the project pytagger. Get it from http://www.liquidx.net/pytagger/
+from tagger import *
+
+#TALB - album
+#TPE1 - artist
+#TIT2 - title.
+#TRCK - track number.
+
+
+def convertTags(file,paramsDict):
+    try:
+        tag = ID3v2(file)
+        framesDict = {x.fid : x for x in tag.frames}
+        for fid, s in [
+                ('TALB',paramsDict['album']),
+                ('TPE1',paramsDict['artist'])
+                ]:
+            if fid in framesDict: frame = framesDict[fid]
+            else: frame = tag.new_frame(fid)
+            frame.set_text(s)
+        tag.commit()
+    except Exception as e:
+        print 'Failed to write %s, with exception %s' %(file,e)
+
+
+
+def inDictAndTrue(k,d):
+    if k in d and d[k]==True:
+        return True
+    else: return False
 
 def nonAscii(s):
     if max([ord(c) for c in s])>0x7E: return True
@@ -23,6 +51,12 @@ def mapIfAscii(items,rootDir,f,paramDict):
         if nonAscii(i): continue
         f(rootDir,i,paramDict)
 
+def mapIfMp3(items,rootDir,f,paramDict):
+    if not f: return
+    for i in items:
+        if len(i)<4 or i[-4:]!='.mp3': continue
+        f(rootDir,i,paramDict)
+
 
 def crunch(rootDir,paramDict=None,fDirs=None,fFiles=None):
     '''Apply fDir(**kwargs) to all the subdirectories of rootDir,
@@ -30,33 +64,36 @@ def crunch(rootDir,paramDict=None,fDirs=None,fFiles=None):
     w = os.walk(rootDir)
     rootDir,dirs,files = w.next()
     mapIfAscii(dirs,rootDir,fDirs,paramDict)
-    mapIfAscii(files,rootDir,fFiles,paramDict)
+    mapIfMp3(files,rootDir,fFiles,paramDict)
 
 
 def applyFileTags(root,f,paramDict):
     artist = paramDict['artist']
     album = paramDict['album']
-    print '%s: Artist: %s, album: %s' %(f,artist,album)
+    if inDictAndTrue('print',paramDict): print '%s: Artist: %s, album: %s' %(f,artist,album)
+    if inDictAndTrue('convertTags',paramDict):
+        convertTags(os.path.join(root,f),paramDict)
 
 def setArtistTag(root,d,paramDict=None):
-    pDict={'artist':d}
+    pDict=paramDict.copy()
+    pDict['artist']=d
     crunch(os.path.join(root,d),pDict,setAlbumTag)
 
 def setAlbumTag(root,d,paramDict,):
-    print '='*20
+    if inDictAndTrue('print',paramDict): print '='*50
     pDict = paramDict.copy()
     pDict['album']=d
     crunch(os.path.join(root,d),pDict,fDirs=None,fFiles=applyFileTags)
 
 
-def crunchRoot(rootDir):
-    crunch(rootDir,fDirs=setArtistTag)
-
-
+def crunchRoot(rootDir,paramDict=None):
+    crunch(rootDir,paramDict,fDirs=setArtistTag)
 
 if __name__ == '__main__':
     if len(sys.argv)<2:
-        #rootDir='.'
-        rootDir=r'C:\Users\guy\Music\Collection'
+        rootDir='.'
+        #rootDir=r'C:\Users\guy\Music\Collection' #for testing
     else: rootDir=sys.argv[1]
-    crunchRoot(rootDir)
+    #TODO - read these parameters from the command line
+    paramDict ={'print' : True, 'convertTags' : False}
+    crunchRoot(rootDir,paramDict)
